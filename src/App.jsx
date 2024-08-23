e103f6ce85d0cb60cbe9aa663fdf9e5fd99570c4
SoleneMarie <solene.plassart@gmail.com>
2024-08-23 19:57:27 +0200

    One counter is working

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,50 @@ import "./App.css";
 
 function App() {
   const [counters, setCounters] = useState([0]);
+  const newArr = [...counters];
   console.log(counters.length);
   console.log(counters);
+  console.log(counters[0]);
+
   return (
     <>
       <h1>React Counter v2</h1>
       <button
         onClick={() => {
-          setCounters([...counters, 0]);
+          if (counters.length < 3) {
+            setCounters([...counters, 0]);
+          }
         }}
       >
         Add counter
       </button>
-      {counters > 0 && counters <= 3 && (
-        <button
-          onClick={() => {
-            setCounters([...counters, 0]);
-          }}
-        >
-          Add counter
-        </button>
-      )}
+
+      {/*Je parcours le tableau counters grâce à la méthode map.
+ Pour chaque case, je récupère la valeur à l'intérieur pour l'afficher.
+ Cette valeur se mettra à jour lors des clicks*/}
+      {counters.map((x) => {
+        return (
+          <div className="button">
+            <button
+              onClick={() => {
+                newArr[0] -= 1;
+                setCounters(newArr);
+              }}
+            >
+              {console.log(x)}-
+            </button>
+            <p>{x}</p>
+            <button
+              onClick={() => {
+                newArr[0] += 1;
+                setCounters(newArr);
+              }}
+            >
+              +
+            </button>
+          </div>
+        );
+      })}
     </>
   );
 }
